4343c5ccb421807b727ac856003b3fdb5a17f4fb
shuai <1627392348@qq.com>
2019-01-14 15:41:29 +0800

    国际化

--- src/xin/yangshuai/javaweb/servlet/I18nServlet.java
+++ src/xin/yangshuai/javaweb/servlet/I18nServlet.java
@@ -1,7 +1,5 @@
 package xin.yangshuai.javaweb.servlet;
 
-import org.apache.tomcat.util.codec.binary.StringUtils;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,25 +22,25 @@ public class I18nServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Locale locale = request.getLocale();
 		String locale1 = request.getParameter("locale");
-		if (locale1 != null && !"".equals(locale1)){
+		if (locale1 != null && !"".equals(locale1)) {
 			String[] split = locale1.split("_");
-			locale = new Locale(split[0],split[1]);
+			locale = new Locale(split[0], split[1]);
 		}
-		request.getSession().setAttribute("locale",locale);
+		request.getSession().setAttribute("locale", locale);
 		ResourceBundle i18n = ResourceBundle.getBundle("i18n", locale);
 		String dateLable = i18n.getString("date");
 		String salLable = i18n.getString("salary");
-		request.setAttribute("dateLable",dateLable);
-		request.setAttribute("salLable",salLable);
+		request.setAttribute("dateLable", dateLable);
+		request.setAttribute("salLable", salLable);
 		DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.FULL, locale);
 		String date = dateFormat.format(new Date());
 		NumberFormat numberFormat = NumberFormat.getCurrencyInstance(locale);
 		String salary = numberFormat.format(12345.67);
-		request.setAttribute("date",date);
-		request.setAttribute("salary",salary);
-		request.setAttribute("date1",new Date());
-		request.setAttribute("salary1",12345.67);
-		request.getRequestDispatcher("/i18n/index.jsp").forward(request,response);
+		request.setAttribute("date", date);
+		request.setAttribute("salary", salary);
+		request.setAttribute("date1", new Date());
+		request.setAttribute("salary1", 12345.67);
+		request.getRequestDispatcher("/i18n/index.jsp").forward(request, response);
 	}
 
 	@Override
